52296e6a291704747bdb018514e978cd0bcb98a3
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-12-02 22:31:23 +0900

    Add vitest, github workflow, setting tweaks

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
+import { defineConfig } from "vitest/config";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [svelte()],
+	test: {
+		watch: false,
+		include: ["src/__tests__/**/*.{test,spec}.{js,ts}"],
+		setupFiles: ["test-setup.ts"],
+	},
 });
