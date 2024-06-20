5ffee996b95234d10aec753a5da463abb30a5d09
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-06-20 13:56:05 +1000

    Update eslint to v9

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+// @ts-expect-error No types
+import js from "@eslint/js";
+import ts from "typescript-eslint";
+import svelte from "eslint-plugin-svelte";
+import globals from "globals";
+
+const config = [
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs["flat/recommended"],
+	...svelte.configs["flat/prettier"],
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+		},
+	},
+	{
+		files: ["**/*.svelte"],
+		languageOptions: {
+			parserOptions: {
+				parser: ts.parser,
+			},
+		},
+	},
+	{
+		ignores: ["build/", ".svelte-kit/", "dist/"],
+	},
+];
+
+export default config;
