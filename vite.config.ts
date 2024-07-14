21f9308f248acb15079f7180ae55e4a9dd11b61c
hugos68 <hugokorteapple@gmail.com>
2024-07-14 15:23:39 +0200

    Fixed prettier pattern, used vite plugin from @testing-library/svelte instead of manually configuring test file.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vitest/config";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { svelteTesting } from "@testing-library/svelte/vite";
 
 export default defineConfig({
-	plugins: [svelte()],
+	plugins: [svelte(), svelteTesting()],
 	test: {
 		watch: false,
 		include: ["tests/**/*.{test,spec}.{js,ts}"],
